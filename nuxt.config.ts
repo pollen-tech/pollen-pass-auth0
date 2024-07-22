3d8410b7a39039f962f950907ed0b1ab3bf874d6
Hung Tran <imhererb@gmail.com>
2024-07-22 23:14:43 +0700

    fix: update API_URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,7 +53,7 @@ export default defineNuxtConfig({
       internalKey: "c3RhZ2luZy1wb2xsZW4udGVjaA==",
       appName: "Pollen Pass",
       appVersion: "2.0.0",
-      API_URL: process.env.API_URL,
+      API_URL: "https://47ekwsnw5d.execute-api.ap-southeast-1.amazonaws.com/dev",
       AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
       AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
       AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
