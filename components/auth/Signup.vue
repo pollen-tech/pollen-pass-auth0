a06fee04bad28040ed7eebf71f0f67812d30fb97
Hung Tran <imhererb@gmail.com>
2024-07-29 15:03:22 +0700

    fix: Redirect issue from SignUp

--- components/auth/Signup.vue
+++ components/auth/Signup.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <CommonLoading v-if="isLoading" :loading="true" />
     <div
       class="d-flex flex-column align-center mx-16"
       :style="{
@@ -28,7 +29,7 @@
         elevation="0"
         class="align-center my-4"
       >
-        <v-form ref="form">
+        <v-form ref="formRef" v-model="valid" lazy-validation>
           <div class="my-4 text-start flex-1-0">
             <label class="font-weight-medium"
               >First Name<span class="red--text">*</span>
@@ -38,7 +39,7 @@
               v-model="item.firstName"
               variant="outlined"
               placeholder="Eg. Kevin"
-              :rules="required"
+              :rules="rules.required"
               class="custom-text-field"
               autocomplete="given-name"
             ></v-text-field>
@@ -53,7 +54,7 @@
               v-model="item.lastName"
               variant="outlined"
               placeholder="Eg. King"
-              :rules="required"
+              :rules="rules.required"
               class="custom-text-field"
               autocomplete="family-name"
             ></v-text-field>
@@ -67,8 +68,9 @@
             <v-text-field
               v-model="item.email"
               variant="outlined"
-              :rules="required"
+              :rules="[...rules.required, rules.email]"
               class="custom-text-field"
+              autocomplete="email"
             ></v-text-field>
           </div>
 
@@ -80,6 +82,8 @@
             @click="onValidateExistEmail()"
             >Continue</v-btn
           >
+          <p class="red--text text-caption text-center">{{ error }}</p>
+          <p class="text-green-lighten-1 text-caption text-center">{{ notification.message }}</p>
         </v-form>
       </v-card>
       <SmallDialog />
@@ -145,19 +149,16 @@ const title = ref("Enter your information");
 const notification = ref({
   title: "How to Start Selling with Pollen's Liquidation Management System",
   desc: "Sign up and get a free LMS account to start listing excess and obsolete inventory, and receive offers from Pollen's verified buyers around the world",
+  message: "",
 });
 const item = ref({ items: [] });
+const valid = ref(false);
 const rules = reactive({
-  required: (value) => !!value || "Required.",
-  min: (v) => v.length >= 8 || "Min 8 characters",
-  emailMatch: () => `The email and password you entered don't match`,
+  required: [(v) => !!v || 'Field is required'],
+  //min: (v) => v.length >= 8 || "Min 8 characters",
+  email: [(v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'],
 });
-const required = [(v) => !!v || "Field is required"];
-const requiredEmail = [
-  (v) =>
-    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
-    "E-mail must be valid",
-];
+const formRef = ref(null);
 const isLoading = ref(false);
 //const showDialog = ref(false);
 const validateCompanyName = ref(0);
@@ -173,8 +174,20 @@ const showDialog = () => {
 };
 
 const onValidateExistEmail = async () => {
+  isLoading.value = true;
+
+  const { valid } = await formRef.value.validate();
+
+  if(!valid) {
+    console.log('Form is invalid');
+    error.value = "Form is invalid";
+    return;
+  } else {
+    error.value = "";
+    notification.value.message = "Form was submitted successfully";
+  }
+
   let email = item.value.email;
-  console.log("onValidateExistEmail -", email);
 
   try {
     const response = await fetch(
@@ -201,19 +214,22 @@ const onValidateExistEmail = async () => {
       console.log("submit");
       submit();
     }
+    isLoading.value = false;
+
   } catch (err) {
+    isLoading.value = false;
     error.value = "Failed to fetch data";
     console.log(err);
   }
 };
 
 const submit = async () => {
-  emit("submit", item.value);
+  emit('submit', item.value);
   const user = {
     email: item.value.email,
     firstName: item.value.firstName,
     lastName: item.value.lastName,
-    channelCode: "POLLEN_PASS",
+    channelCode: 'POLLEN_PASS',
   };
   userStore.setUser(user);
   console.log(user);
@@ -238,8 +254,10 @@ const submit = async () => {
     if (data.value.status_code === "LOGIN_ERROR") {
       throw new Error("LOGIN ERROR");
     }
+    isLoading.value = false;
     navigateTo("/auth/verification");
   } catch (err) {
+    isLoading.value = false;
     error.value = "Failed to fetch data";
     console.log(err);
   }
