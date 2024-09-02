1531e2b4d1e45860781fb45bba7d3cd9a38c1f85
katrineperez <katrine@pollen.tech>
2024-09-02 17:44:51 +0800

    fix: add channel login content

--- components/auth/SideBar.vue
+++ components/auth/SideBar.vue
@@ -14,33 +14,50 @@
       More Optionality,<br />
       More Sustainability
     </p>
-    <ul class="pl-4 pt-4" style="font-size: 18px; color: white">
-      <li v-for="text in context" :key="text" class="pb-4">
-        {{ text }}
+
+    <ul
+      v-if="channel == 'CH_LMS'"
+      class="pl-4 pt-4"
+      style="font-size: 18px; color: white"
+    >
+      <li v-for="lms in lms_context" :key="lms" class="pb-4">
+        {{ lms }}
       </li>
     </ul>
+    <ul v-else class="pl-4 pt-4" style="font-size: 18px; color: white">
+      <li v-for="direct in direct_context" :key="direct" class="pb-4">
+        {{ direct }}
+      </li>
+    </ul>
+
     <p class="text-start text-white font-weight-medium mt-4">
       - Powered by
       <a
-        href=" https://www.pollen.tech"
+        href="https://pollen.tech/"
         class="text-decoration-underline font-weight-bold"
         style="color: #fde68a"
-        >Pollen Pass</a
+        >Pollen.tech</a
       >
     </p>
   </v-sheet>
 </template>
 
 <script setup>
-const { context } = defineProps({
-  context: {
-    type: Array,
-    required: false,
-    default: () => [
-      "Value your inventory with market data",
-      "Liquidate globally and multichannel",
-      "Reduce business waste",
-    ],
-  },
-});
+import { useAuth } from "@/composables/auth0";
+const auth = useAuth();
+
+const channel = computed(() => auth.get_channel());
+
+const lms_context = ref([
+  "Value your inventory with market data",
+  "Liquidate globally and multichannel",
+  "Reduce business waste",
+]);
+const direct_context = ref([
+  "Pollen Pass Exclusive",
+  "100% Authentic",
+  "Known & Loved Brands",
+  "Direct from Global Manufacturers",
+  "Regularly Refreshed",
+]);
 </script>
