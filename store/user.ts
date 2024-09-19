a54a155b64b97c4635f7f2230d5916a62d7bfce0
katrineperez <katrine@pollen.tech>
2024-09-19 15:09:47 +0800

    fix: integrate api to user store

--- store/user.ts
+++ store/user.ts
@@ -8,6 +8,7 @@ interface User {
   lastName: string;
   channelCode: string;
   phoneNumber: string;
+  phone: string;
   countryCode: string;
 }
 
@@ -22,14 +23,37 @@ export const useUserStore = defineStore("user", {
     setUser(user: User) {
       this.user = { ...this.user, ...user };
       localStorage.setItem("email", user.email);
+      localStorage.setItem("user_signup", JSON.stringify(user));
     },
     getUser(): User | null {
       return this.user;
+    },
+    cleanupUser() {
+      localStorage.removeItem("user_signup");
+    },
+    getUserLocalStorage() {
+      const user = localStorage.getItem("user_signup");
+      return user ? JSON.parse(user) : user;
     },
 
     async get_user_profile(param: any) {
       const req = await userOnboardApi(`/users/${param}`);
       this.user_profile = req.data || req;
+      return req;
+    },
+
+    async validate_email_exist(param_email: any) {
+      const req = await userOnboardApi(
+        `/users/pollen-pass-by-email/${param_email}`
+      );
+      return req;
+    },
+
+    async verify_passwordless_email_login(param_email: any) {
+      const req = await userOnboardApi(
+        `/auth0/password-less-email-login/${param_email}`,
+        "POST"
+      );
       return req;
     },
   },
